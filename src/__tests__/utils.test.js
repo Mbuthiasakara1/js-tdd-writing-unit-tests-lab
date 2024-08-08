ab6314dea26789356007988544803051b1b18df7
Mbuthiasakara1 <mbuthiaryne@gmail.com>
2024-08-09 00:43:59 +0300

    testing lab on palindrome

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,17 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+  it("checks if word is a palindrome", () => {
+    const word = "mom";
+    const palindrome = isPalindrome(word);
+    expect(palindrome).toBe(true);
+  });
+
+  it("checks if word is not a palindrome", () => {
+    const word = "car";
+    const palindrome = isPalindrome(word);
+    expect(palindrome).toBe(false);
+  });
+});
+
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,14 @@
 // Your code here
+export function isPalindrome(word) {
+  let rev = word.split("").reverse().join("");
+  if (rev == word) {
+    return true;
+  }
+  return false;
+}
+
+let word1 = "mom";
+let word2 = "car";
+
+isPalindrome(word1);
+isPalindrome(word2);
